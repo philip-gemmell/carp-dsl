c79c9d07a511dd4890e9127aad480362ebf8eb80
Philip Gemmell <philip.gemmell@gmail.com>
2022-02-04 11:41:44 +0000

    Add centre commands for mesh
    Add option to apply a different centre for mesh construction. Note, I'm
    not entirely sure what units the centre is meant to be supplied in - I'm
    assuming that it's the same as the mesh size, and thus cm

--- carp.py
+++ carp.py
@@ -47,10 +47,11 @@ class Simulation(object):
     def __init__(self):
         # Mesh specific commands
         self.mesh_name = ""
-        self.size = [1, 1, 1]
-        self.resolution = [100, 100, 100]
-        self.size_factor = 0.1          # mesher expects this input to be in cm, whereas our default is mm
-        self.resolution_factor = 1000   # mesher expects this input to be in um, whereas our default is mm
+        self.mesh_size = [1, 1, 1]
+        self.mesh_resolution = [100, 100, 100]
+        self.mesh_size_factor = 0.1          # mesher expects this input to be in cm, whereas our default is mm
+        self.mesh_resolution_factor = 1000   # mesher expects this input to be in um, whereas our default is mm
+        self.mesh_centre = [0.0, 0.0, 0.0]
 
         # Stimulus commands
         self.n_stim = 0
@@ -74,38 +75,47 @@ class Simulation(object):
                 cmd_func = print
 
             elif cmd.__class__.__name__ == "SetMesh":
-                if cmd.setting.lower() == "size":
-                    self.size = [cmd.x, cmd.y, cmd.z]
+                if cmd.setting.lower() == "mesh_size":
+                    self.mesh_size = [cmd.x, cmd.y, cmd.z]
 
-                elif cmd.setting.lower() == "resolution":
-                    self.resolution = [cmd.x, cmd.y, cmd.z]
+                elif cmd.setting.lower() == "mesh_resolution":
+                    self.mesh_resolution = [cmd.x, cmd.y, cmd.z]
 
-                elif cmd.setting.lower() == "units":
-                    # mesher expects size for baths, tissue, etc., to be in units of cm,
-                    # while it expects the resolution to be in units of um.
+                elif cmd.setting.lower() == "units to be":
+                    # mesher expects mesh_size for baths, tissue, etc., to be in units of cm,
+                    # while it expects the mesh_resolution to be in units of um.
                     if cmd.units == "mm":
-                        self.size_factor = 0.1
-                        self.resolution_factor = 100
+                        self.mesh_size_factor = 0.1
+                        self.mesh_resolution_factor = 100
                     elif cmd.units == "um":
-                        self.size_factor = 100
-                        self.resolution_factor = 1
+                        self.mesh_size_factor = 100
+                        self.mesh_resolution_factor = 1
+
+                elif cmd.setting.lower() == "centre at":
+                    self.mesh_centre = [cmd.x, cmd.y, cmd.z]
+
+                else:
+                    raise Exception('Unexpected value given for MeshSetCmd')
 
             elif cmd.__class__.__name__ == "CreateMesh":
                 print("Creating mesh...")
                 if not os.path.isdir(cmd.folder):
                     os.makedirs(cmd.folder)
-                size = [i_size*self.size_factor for i_size in self.size]
-                resolution = [i_res*self.resolution_factor for i_res in self.resolution]
+                size = [i_size * self.mesh_size_factor for i_size in self.mesh_size]
+                resolution = [i_res * self.mesh_resolution_factor for i_res in self.mesh_resolution]
+                centre = [c * self.mesh_size_factor for c in self.mesh_centre]
                 self.mesh_name = os.path.join(cmd.folder, cmd.name)
                 cmd_mesh = "/usr/local/bin/mesher" + \
-                           " -size[0] " + str(size[0]) + \
-                           " -size[1] " + str(size[1]) + \
-                           " -size[2] " + str(size[2]) + \
+                           " -mesh_size[0] " + str(size[0]) + \
+                           " -mesh_size[1] " + str(size[1]) + \
+                           " -mesh_size[2] " + str(size[2]) + \
                            " -bath[0] -0.0 -bath[1] -0.0 -bath[2] -0.0" + \
-                           " -center[0] 0.0 -center[1] 0.0 -center[2] 0.0" + \
-                           " -resolution[0] " + str(resolution[0]) + \
-                           " -resolution[1] " + str(resolution[1]) + \
-                           " -resolution[2] " + str(resolution[2]) + \
+                           " -center[0] " + str(centre[0]) + \
+                           " -center[1] " + str(centre[1]) + \
+                           " -center[2] " + str(centre[2]) + \
+                           " -mesh_resolution[0] " + str(resolution[0]) + \
+                           " -mesh_resolution[1] " + str(resolution[1]) + \
+                           " -mesh_resolution[2] " + str(resolution[2]) + \
                            " -mesh " + str(self.mesh_name) + \
                            " -Elem3D 0" + \
                            " -fibers.rotEndo 0.0 -fibers.rotEpi 0.0 -fibers.sheetEndo 90.0 -fibers.sheetEpi 90.0" + \
